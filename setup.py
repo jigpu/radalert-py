9de5f7a0fbc0bae64ab723e816c42fc1b40a51a8
Jason Gerecke <killertofu@gmail.com>
2020-12-07 07:57:28 -0800

    Replace manual setup.py with pbr dependency
    I can stuff info into a configuration file now and can let the git repo's
    tags be used as the source of version info. Sweet!

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 import setuptools
 
-with open('requirements.txt', 'r') as f:
-    install_requires = f.read().splitlines()
-
-setuptools.setup(name='radalert',
-                 packages=['radalert'],
-                 install_requires=install_requires)
+setuptools.setup(setup_requires=['pbr'], pbr = True)
